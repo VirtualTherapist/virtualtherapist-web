6a2de6b9f40716f9a8bb440a750842808d4f48bb
Auke Willem Oosterhoff <awoosterhoff@gmail.com>
2014-10-20 12:54:10 +0200

    Added login tests.

--- test/RoutesTest.java
+++ test/RoutesTest.java
@@ -1,5 +1,16 @@
-/**
+import org.junit.Test;
+import play.mvc.Result;
+
+import static play.test.Helpers.*;
+import static org.fest.assertions.Assertions.assertThat;
+
+ /**
  * Created by wahid on 10/10/14.
  */
 public class RoutesTest {
+   @Test
+   public void testGetLogin() {
+       Result result = route(fakeRequest(GET, "/login"));
+       assertThat(result).isNotNull();
+   }
 }
