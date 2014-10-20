cf135d4596ea62ecca4e5bfdd51be64bd80a241f
Auke Willem Oosterhoff <awoosterhoff@gmail.com>
2014-10-20 17:00:21 +0200

    Added a few tests to test routes..

--- test/RoutesTest.java
+++ test/RoutesTest.java
@@ -1,5 +1,10 @@
 import org.junit.Test;
+import org.junit.Ignore;
 import play.mvc.Result;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+import java.util.Arrays;
 
 import static play.test.Helpers.*;
 import static org.fest.assertions.Assertions.assertThat;
@@ -8,9 +13,80 @@ import static org.fest.assertions.Assertions.assertThat;
  * Created by wahid on 10/10/14.
  */
 public class RoutesTest {
-   @Test
-   public void testGetLogin() {
-       Result result = route(fakeRequest(GET, "/login"));
-       assertThat(result).isNotNull();
-   }
+
+    
+    // List with all URLS 
+    private List<String> getUrls = Arrays.asList(
+            "/vragen",
+            "/vragen/all",
+            "/vragen/delete/1",
+            "/antwoord/delete/1",
+            "/login",
+            "/register",
+            "/logout"
+            //"/analyse"                // Not Implemented
+    );
+
+    /** 
+     * Iterate over list with urls and perform a fake HTTP GET request on each
+     * URL, checking if response is not null.
+     */
+    @Test
+    public void getUrlTest() {
+        running(fakeApplication(), new Runnable() {
+            public void run() {
+                for (String url: getUrls) {
+                    Result result = route(fakeRequest(GET, url));
+                    assertThat(result).isNotNull();
+                }
+            }
+        });
+    }
+
+    @Test
+    @Ignore("Not implemented.")
+    public void getUserTest() {
+       running(fakeApplication(), new Runnable() {
+           public void run() {
+               Result result = route(fakeRequest(GET, "/gebruikers/all"));
+               assertThat(result).isNotNull();
+           }
+        });
+    }
+
+    @Test
+    @Ignore("Not implemented.")
+    public void postUserTest() {
+       running(fakeApplication(), new Runnable() {
+           public void run() {
+              Result result = route(fakeRequest(GET, "/gebruikers/1"));
+              assertThat(result).isNotNull();
+           }
+        });
+    }
+
+    @Test
+    public void postQuestionTest() {
+       running(fakeApplication(), new Runnable() {
+           public void run() {
+              Result result = route(fakeRequest(POST, "/vragen"));
+              assertThat(result).isNotNull();
+           }
+        });
+    }
+
+    @Test
+    public void postSaveQuestionTest() {
+        running(fakeApplication(), new Runnable() {
+            public void run() {
+                Map<String, String> map = new HashMap<>();
+                map.put("question", "Is this a question?");
+                map.put("answer", "This this is anwer.");
+
+
+                Result result = route(fakeRequest(POST, "/vragen/opslaan").withFormUrlEncodedBody(map));
+                assertThat(result).isNotNull();
+            }
+        });
+    }
 }
