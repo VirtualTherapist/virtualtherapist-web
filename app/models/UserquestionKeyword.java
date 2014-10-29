49959f3d3fbc210a656c4be4fc3cf3c1fc900aaa
Vasco de Krijger <vascokiller@live.nl>
2014-10-29 13:22:25 +0100

    Feature 68 en 69 afgerond, je krijgt nu questions aan de hand van de keywords

--- app/models/UserquestionKeyword.java
+++ app/models/UserquestionKeyword.java
@@ -4,19 +4,23 @@ import play.data.validation.Constraints;
 import play.db.ebean.Model;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
 /**
  * Created by Akatchi on 20-10-2014.
  */
 @Entity
-public class UserquestionKeyword extends Model
+public class UserQuestionKeyword extends Model
 {
+    @Id
+    public Integer id;
+
     @Constraints.Required
     @OneToOne
     public UserQuestion userquestion;
 
     @Constraints.Required
     @OneToOne
-    public Keyword keyword;
+    public KeywordCategory keywordCategory;
 }
