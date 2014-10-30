216cef245e1c67a87f8cb7dfd0828b352997437b
Jeroen Lammerts <jeroenlammerts@gmail.com>
2014-10-30 13:20:16 +0100

    Profile page

--- app/controllers/UserController.java
+++ app/controllers/UserController.java
@@ -5,10 +5,12 @@ import models.User;
 import play.api.libs.Crypto;
 import play.mvc.Controller;
 import play.mvc.Result;
-
+import play.data.DynamicForm;
+import utils.HashUtil;
 import java.util.List;
 import views.html.*;
 
+import static play.data.Form.form;
 import static play.libs.Json.toJson;
 
 /**
@@ -36,4 +38,33 @@ public class UserController extends Controller
         List<User> allUsers = Ebean.find(User.class).findList();
         return redirect("/gebruikers/all");
     }
+
+    public static Result showProfile()
+    {
+        User user= Ebean.find(User.class).where().eq("email", Crypto.decryptAES(session(Crypto.encryptAES("email")))).findUnique();
+        return ok(profile.render(user, Crypto.decryptAES(session(Crypto.encryptAES("firstname"))), Crypto.decryptAES(session(Crypto.encryptAES("lastname"))), "", ""));
+    }
+
+    public static Result saveProfile()
+    {
+        DynamicForm userForm = form().bindFromRequest();
+
+        String email = Crypto.decryptAES(session(Crypto.encryptAES("email")));
+
+        User user= Ebean.find(User.class).where().eq("email", email).findUnique();
+        user.first_name = userForm.get("first_name");
+        user.last_name = userForm.get("last_name");
+
+        String password = userForm.get("password");
+        if(!password.isEmpty()){
+            String hash = HashUtil.createHash(email, userForm.get("password"));
+            user.password = hash;
+        }
+
+        user.save();
+
+        return redirect(controllers.routes.UserController.showProfile());
+    }
+
+
 }
