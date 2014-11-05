a94d3d4abce9dbb2197ce11850d9703366c4a5b1
Auke Willem Oosterhoff <awoosterhoff@gmail.com>
2014-11-05 11:31:39 +0100

    #126 Implemented answer/question ratio.

--- app/controllers/AnalysisController.java
+++ app/controllers/AnalysisController.java
@@ -3,6 +3,8 @@ package controllers;
 
 import com.avaje.ebean.Ebean;
 import models.User;
+import models.ChatLine;
+import models.Answer;
 import play.api.libs.Crypto;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -12,7 +14,6 @@ import views.html.*;
  * Created by bas on 9-10-14.
  */
 public class AnalysisController extends Controller {
-
     public static Result analysisPageForUser(Integer userId) {
         User user = null;
         if(userId != null) {
@@ -20,15 +21,32 @@ public class AnalysisController extends Controller {
         }
         return ok(analysis.render("Analyse", user,
                 Crypto.decryptAES(session(Crypto.encryptAES("firstname"))),
-                Crypto.decryptAES(session(Crypto.encryptAES("lastname"))), "", ""));
+                Crypto.decryptAES(session(Crypto.encryptAES("lastname"))), "", "",
+                0, 0));
         //return ok();
+
     }
 
     public static Result analysisPage() {
+        int amountOfQuestions = Ebean.find(ChatLine.class).findRowCount();
+        Answer answer = Ebean.find(Answer.class)
+            .where()
+            .eq("answer", "Geen antwoord gevonden")
+            .findUnique();
+
+        int amountOfUnansweredQuestions = 0;
+        if (answer != null) {
+            int amountOfUnAnsweredQuestions = Ebean.find(ChatLine.class)
+                .where()
+                .eq("answer_id", answer.id)
+                .findRowCount();
+        }
+
         //return ok();
         return ok(analysis.render("Analyse", null,
                 Crypto.decryptAES(session(Crypto.encryptAES("firstname"))),
-                Crypto.decryptAES(session(Crypto.encryptAES("lastname"))), "", ""));
+                Crypto.decryptAES(session(Crypto.encryptAES("lastname"))), "", "",
+                amountOfQuestions, amountOfUnansweredQuestions));
     }
 
     //public static Result keywordTrendPage() {
